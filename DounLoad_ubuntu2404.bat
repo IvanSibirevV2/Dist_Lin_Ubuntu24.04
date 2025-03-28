dd2c4ac90b65cca9cc3dd9f235b49278b5b8c8e0
йцу <>
2025-03-28 14:51:56 +0300

    AutoCommit_28 марта 2025 г. 14:51:56_SibNout2023

---
+++ DounLoad_ubuntu2404.bat
@@ -0,0 +1,483 @@
+:::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+cd %~dp0
+title %~0
+:::::::::::::::::::::::::::::::::::::::::::::
+call :qwe ubuntu2404.7z.001
+call :qwe ubuntu2404.7z.002
+call :qwe ubuntu2404.7z.003
+call :qwe ubuntu2404.7z.004
+call :qwe ubuntu2404.7z.005
+call :qwe ubuntu2404.7z.006
+call :qwe ubuntu2404.7z.007
+call :qwe ubuntu2404.7z.008
+call :qwe ubuntu2404.7z.009
+call :qwe ubuntu2404.7z.010
+call :qwe ubuntu2404.7z.011
+call :qwe ubuntu2404.7z.012
+call :qwe ubuntu2404.7z.013
+call :qwe ubuntu2404.7z.014
+call :qwe ubuntu2404.7z.015
+call :qwe ubuntu2404.7z.016
+call :qwe ubuntu2404.7z.017
+call :qwe ubuntu2404.7z.018
+call :qwe ubuntu2404.7z.019
+call :qwe ubuntu2404.7z.020
+call :qwe ubuntu2404.7z.021
+call :qwe ubuntu2404.7z.022
+call :qwe ubuntu2404.7z.023
+call :qwe ubuntu2404.7z.024
+call :qwe ubuntu2404.7z.025
+call :qwe ubuntu2404.7z.026
+call :qwe ubuntu2404.7z.027
+call :qwe ubuntu2404.7z.028
+call :qwe ubuntu2404.7z.029
+call :qwe ubuntu2404.7z.030
+call :qwe ubuntu2404.7z.031
+call :qwe ubuntu2404.7z.032
+call :qwe ubuntu2404.7z.033
+call :qwe ubuntu2404.7z.034
+call :qwe ubuntu2404.7z.035
+call :qwe ubuntu2404.7z.036
+call :qwe ubuntu2404.7z.037
+call :qwe ubuntu2404.7z.038
+call :qwe ubuntu2404.7z.039
+call :qwe ubuntu2404.7z.040
+call :qwe ubuntu2404.7z.041
+call :qwe ubuntu2404.7z.042
+call :qwe ubuntu2404.7z.043
+call :qwe ubuntu2404.7z.044
+call :qwe ubuntu2404.7z.045
+call :qwe ubuntu2404.7z.046
+call :qwe ubuntu2404.7z.047
+call :qwe ubuntu2404.7z.048
+call :qwe ubuntu2404.7z.049
+call :qwe ubuntu2404.7z.050
+call :qwe ubuntu2404.7z.051
+call :qwe ubuntu2404.7z.052
+call :qwe ubuntu2404.7z.053
+call :qwe ubuntu2404.7z.054
+call :qwe ubuntu2404.7z.055
+call :qwe ubuntu2404.7z.056
+call :qwe ubuntu2404.7z.057
+call :qwe ubuntu2404.7z.058
+call :qwe ubuntu2404.7z.059
+call :qwe ubuntu2404.7z.060
+call :qwe ubuntu2404.7z.061
+call :qwe ubuntu2404.7z.062
+call :qwe ubuntu2404.7z.063
+call :qwe ubuntu2404.7z.064
+call :qwe ubuntu2404.7z.065
+call :qwe ubuntu2404.7z.066
+call :qwe ubuntu2404.7z.067
+call :qwe ubuntu2404.7z.068
+call :qwe ubuntu2404.7z.069
+call :qwe ubuntu2404.7z.070
+call :qwe ubuntu2404.7z.071
+call :qwe ubuntu2404.7z.072
+call :qwe ubuntu2404.7z.073
+call :qwe ubuntu2404.7z.074
+call :qwe ubuntu2404.7z.075
+call :qwe ubuntu2404.7z.076
+call :qwe ubuntu2404.7z.077
+call :qwe ubuntu2404.7z.078
+call :qwe ubuntu2404.7z.079
+call :qwe ubuntu2404.7z.080
+call :qwe ubuntu2404.7z.081
+call :qwe ubuntu2404.7z.082
+call :qwe ubuntu2404.7z.083
+call :qwe ubuntu2404.7z.084
+call :qwe ubuntu2404.7z.085
+call :qwe ubuntu2404.7z.086
+call :qwe ubuntu2404.7z.087
+call :qwe ubuntu2404.7z.088
+call :qwe ubuntu2404.7z.089
+call :qwe ubuntu2404.7z.090
+call :qwe ubuntu2404.7z.091
+call :qwe ubuntu2404.7z.092
+call :qwe ubuntu2404.7z.093
+call :qwe ubuntu2404.7z.094
+call :qwe ubuntu2404.7z.095
+call :qwe ubuntu2404.7z.096
+call :qwe ubuntu2404.7z.097
+call :qwe ubuntu2404.7z.098
+call :qwe ubuntu2404.7z.099
+call :qwe ubuntu2404.7z.100
+call :qwe ubuntu2404.7z.101
+call :qwe ubuntu2404.7z.102
+call :qwe ubuntu2404.7z.103
+call :qwe ubuntu2404.7z.104
+call :qwe ubuntu2404.7z.105
+call :qwe ubuntu2404.7z.106
+call :qwe ubuntu2404.7z.107
+call :qwe ubuntu2404.7z.108
+call :qwe ubuntu2404.7z.109
+call :qwe ubuntu2404.7z.110
+call :qwe ubuntu2404.7z.111
+call :qwe ubuntu2404.7z.112
+call :qwe ubuntu2404.7z.113
+call :qwe ubuntu2404.7z.114
+call :qwe ubuntu2404.7z.115
+call :qwe ubuntu2404.7z.116
+call :qwe ubuntu2404.7z.117
+call :qwe ubuntu2404.7z.118
+call :qwe ubuntu2404.7z.119
+call :qwe ubuntu2404.7z.120
+call :qwe ubuntu2404.7z.121
+call :qwe ubuntu2404.7z.122
+call :qwe ubuntu2404.7z.123
+call :qwe ubuntu2404.7z.124
+call :qwe ubuntu2404.7z.125
+call :qwe ubuntu2404.7z.126
+call :qwe ubuntu2404.7z.127
+call :qwe ubuntu2404.7z.128
+call :qwe ubuntu2404.7z.129
+call :qwe ubuntu2404.7z.130
+call :qwe ubuntu2404.7z.131
+call :qwe ubuntu2404.7z.132
+call :qwe ubuntu2404.7z.133
+call :qwe ubuntu2404.7z.134
+call :qwe ubuntu2404.7z.135
+call :qwe ubuntu2404.7z.136
+call :qwe ubuntu2404.7z.137
+call :qwe ubuntu2404.7z.138
+call :qwe ubuntu2404.7z.139
+call :qwe ubuntu2404.7z.140
+call :qwe ubuntu2404.7z.141
+call :qwe ubuntu2404.7z.142
+call :qwe ubuntu2404.7z.143
+call :qwe ubuntu2404.7z.144
+call :qwe ubuntu2404.7z.145
+call :qwe ubuntu2404.7z.146
+call :qwe ubuntu2404.7z.147
+call :qwe ubuntu2404.7z.148
+call :qwe ubuntu2404.7z.149
+call :qwe ubuntu2404.7z.150
+call :qwe ubuntu2404.7z.151
+call :qwe ubuntu2404.7z.152
+call :qwe ubuntu2404.7z.153
+call :qwe ubuntu2404.7z.154
+call :qwe ubuntu2404.7z.155
+call :qwe ubuntu2404.7z.156
+call :qwe ubuntu2404.7z.157
+call :qwe ubuntu2404.7z.158
+call :qwe ubuntu2404.7z.159
+call :qwe ubuntu2404.7z.160
+call :qwe ubuntu2404.7z.161
+call :qwe ubuntu2404.7z.162
+call :qwe ubuntu2404.7z.163
+call :qwe ubuntu2404.7z.164
+call :qwe ubuntu2404.7z.165
+call :qwe ubuntu2404.7z.166
+call :qwe ubuntu2404.7z.167
+call :qwe ubuntu2404.7z.168
+call :qwe ubuntu2404.7z.169
+call :qwe ubuntu2404.7z.170
+call :qwe ubuntu2404.7z.171
+call :qwe ubuntu2404.7z.172
+call :qwe ubuntu2404.7z.173
+call :qwe ubuntu2404.7z.174
+call :qwe ubuntu2404.7z.175
+call :qwe ubuntu2404.7z.176
+call :qwe ubuntu2404.7z.177
+call :qwe ubuntu2404.7z.178
+call :qwe ubuntu2404.7z.179
+call :qwe ubuntu2404.7z.180
+call :qwe ubuntu2404.7z.181
+call :qwe ubuntu2404.7z.182
+call :qwe ubuntu2404.7z.183
+call :qwe ubuntu2404.7z.184
+call :qwe ubuntu2404.7z.185
+call :qwe ubuntu2404.7z.186
+call :qwe ubuntu2404.7z.187
+call :qwe ubuntu2404.7z.188
+call :qwe ubuntu2404.7z.189
+call :qwe ubuntu2404.7z.190
+call :qwe ubuntu2404.7z.191
+call :qwe ubuntu2404.7z.192
+call :qwe ubuntu2404.7z.193
+call :qwe ubuntu2404.7z.194
+call :qwe ubuntu2404.7z.195
+call :qwe ubuntu2404.7z.196
+call :qwe ubuntu2404.7z.197
+call :qwe ubuntu2404.7z.198
+call :qwe ubuntu2404.7z.199
+call :qwe ubuntu2404.7z.200
+call :qwe ubuntu2404.7z.201
+call :qwe ubuntu2404.7z.202
+call :qwe ubuntu2404.7z.203
+call :qwe ubuntu2404.7z.204
+call :qwe ubuntu2404.7z.205
+call :qwe ubuntu2404.7z.206
+call :qwe ubuntu2404.7z.207
+call :qwe ubuntu2404.7z.208
+call :qwe ubuntu2404.7z.209
+call :qwe ubuntu2404.7z.210
+call :qwe ubuntu2404.7z.211
+call :qwe ubuntu2404.7z.212
+call :qwe ubuntu2404.7z.213
+call :qwe ubuntu2404.7z.214
+call :qwe ubuntu2404.7z.215
+call :qwe ubuntu2404.7z.216
+call :qwe ubuntu2404.7z.217
+call :qwe ubuntu2404.7z.218
+call :qwe ubuntu2404.7z.219
+call :qwe ubuntu2404.7z.220
+call :qwe ubuntu2404.7z.221
+call :qwe ubuntu2404.7z.222
+call :qwe ubuntu2404.7z.223
+call :qwe ubuntu2404.7z.224
+call :qwe ubuntu2404.7z.225
+call :qwe ubuntu2404.7z.226
+call :qwe ubuntu2404.7z.227
+call :qwe ubuntu2404.7z.228
+call :qwe ubuntu2404.7z.229
+call :qwe ubuntu2404.7z.230
+call :qwe ubuntu2404.7z.231
+call :qwe ubuntu2404.exe
+call ubuntu2404.exe
+del ubuntu2404.7z.001
+del ubuntu2404.7z.002
+del ubuntu2404.7z.003
+del ubuntu2404.7z.004
+del ubuntu2404.7z.005
+del ubuntu2404.7z.006
+del ubuntu2404.7z.007
+del ubuntu2404.7z.008
+del ubuntu2404.7z.009
+del ubuntu2404.7z.010
+del ubuntu2404.7z.011
+del ubuntu2404.7z.012
+del ubuntu2404.7z.013
+del ubuntu2404.7z.014
+del ubuntu2404.7z.015
+del ubuntu2404.7z.016
+del ubuntu2404.7z.017
+del ubuntu2404.7z.018
+del ubuntu2404.7z.019
+del ubuntu2404.7z.020
+del ubuntu2404.7z.021
+del ubuntu2404.7z.022
+del ubuntu2404.7z.023
+del ubuntu2404.7z.024
+del ubuntu2404.7z.025
+del ubuntu2404.7z.026
+del ubuntu2404.7z.027
+del ubuntu2404.7z.028
+del ubuntu2404.7z.029
+del ubuntu2404.7z.030
+del ubuntu2404.7z.031
+del ubuntu2404.7z.032
+del ubuntu2404.7z.033
+del ubuntu2404.7z.034
+del ubuntu2404.7z.035
+del ubuntu2404.7z.036
+del ubuntu2404.7z.037
+del ubuntu2404.7z.038
+del ubuntu2404.7z.039
+del ubuntu2404.7z.040
+del ubuntu2404.7z.041
+del ubuntu2404.7z.042
+del ubuntu2404.7z.043
+del ubuntu2404.7z.044
+del ubuntu2404.7z.045
+del ubuntu2404.7z.046
+del ubuntu2404.7z.047
+del ubuntu2404.7z.048
+del ubuntu2404.7z.049
+del ubuntu2404.7z.050
+del ubuntu2404.7z.051
+del ubuntu2404.7z.052
+del ubuntu2404.7z.053
+del ubuntu2404.7z.054
+del ubuntu2404.7z.055
+del ubuntu2404.7z.056
+del ubuntu2404.7z.057
+del ubuntu2404.7z.058
+del ubuntu2404.7z.059
+del ubuntu2404.7z.060
+del ubuntu2404.7z.061
+del ubuntu2404.7z.062
+del ubuntu2404.7z.063
+del ubuntu2404.7z.064
+del ubuntu2404.7z.065
+del ubuntu2404.7z.066
+del ubuntu2404.7z.067
+del ubuntu2404.7z.068
+del ubuntu2404.7z.069
+del ubuntu2404.7z.070
+del ubuntu2404.7z.071
+del ubuntu2404.7z.072
+del ubuntu2404.7z.073
+del ubuntu2404.7z.074
+del ubuntu2404.7z.075
+del ubuntu2404.7z.076
+del ubuntu2404.7z.077
+del ubuntu2404.7z.078
+del ubuntu2404.7z.079
+del ubuntu2404.7z.080
+del ubuntu2404.7z.081
+del ubuntu2404.7z.082
+del ubuntu2404.7z.083
+del ubuntu2404.7z.084
+del ubuntu2404.7z.085
+del ubuntu2404.7z.086
+del ubuntu2404.7z.087
+del ubuntu2404.7z.088
+del ubuntu2404.7z.089
+del ubuntu2404.7z.090
+del ubuntu2404.7z.091
+del ubuntu2404.7z.092
+del ubuntu2404.7z.093
+del ubuntu2404.7z.094
+del ubuntu2404.7z.095
+del ubuntu2404.7z.096
+del ubuntu2404.7z.097
+del ubuntu2404.7z.098
+del ubuntu2404.7z.099
+del ubuntu2404.7z.100
+del ubuntu2404.7z.101
+del ubuntu2404.7z.102
+del ubuntu2404.7z.103
+del ubuntu2404.7z.104
+del ubuntu2404.7z.105
+del ubuntu2404.7z.106
+del ubuntu2404.7z.107
+del ubuntu2404.7z.108
+del ubuntu2404.7z.109
+del ubuntu2404.7z.110
+del ubuntu2404.7z.111
+del ubuntu2404.7z.112
+del ubuntu2404.7z.113
+del ubuntu2404.7z.114
+del ubuntu2404.7z.115
+del ubuntu2404.7z.116
+del ubuntu2404.7z.117
+del ubuntu2404.7z.118
+del ubuntu2404.7z.119
+del ubuntu2404.7z.120
+del ubuntu2404.7z.121
+del ubuntu2404.7z.122
+del ubuntu2404.7z.123
+del ubuntu2404.7z.124
+del ubuntu2404.7z.125
+del ubuntu2404.7z.126
+del ubuntu2404.7z.127
+del ubuntu2404.7z.128
+del ubuntu2404.7z.129
+del ubuntu2404.7z.130
+del ubuntu2404.7z.131
+del ubuntu2404.7z.132
+del ubuntu2404.7z.133
+del ubuntu2404.7z.134
+del ubuntu2404.7z.135
+del ubuntu2404.7z.136
+del ubuntu2404.7z.137
+del ubuntu2404.7z.138
+del ubuntu2404.7z.139
+del ubuntu2404.7z.140
+del ubuntu2404.7z.141
+del ubuntu2404.7z.142
+del ubuntu2404.7z.143
+del ubuntu2404.7z.144
+del ubuntu2404.7z.145
+del ubuntu2404.7z.146
+del ubuntu2404.7z.147
+del ubuntu2404.7z.148
+del ubuntu2404.7z.149
+del ubuntu2404.7z.150
+del ubuntu2404.7z.151
+del ubuntu2404.7z.152
+del ubuntu2404.7z.153
+del ubuntu2404.7z.154
+del ubuntu2404.7z.155
+del ubuntu2404.7z.156
+del ubuntu2404.7z.157
+del ubuntu2404.7z.158
+del ubuntu2404.7z.159
+del ubuntu2404.7z.160
+del ubuntu2404.7z.161
+del ubuntu2404.7z.162
+del ubuntu2404.7z.163
+del ubuntu2404.7z.164
+del ubuntu2404.7z.165
+del ubuntu2404.7z.166
+del ubuntu2404.7z.167
+del ubuntu2404.7z.168
+del ubuntu2404.7z.169
+del ubuntu2404.7z.170
+del ubuntu2404.7z.171
+del ubuntu2404.7z.172
+del ubuntu2404.7z.173
+del ubuntu2404.7z.174
+del ubuntu2404.7z.175
+del ubuntu2404.7z.176
+del ubuntu2404.7z.177
+del ubuntu2404.7z.178
+del ubuntu2404.7z.179
+del ubuntu2404.7z.180
+del ubuntu2404.7z.181
+del ubuntu2404.7z.182
+del ubuntu2404.7z.183
+del ubuntu2404.7z.184
+del ubuntu2404.7z.185
+del ubuntu2404.7z.186
+del ubuntu2404.7z.187
+del ubuntu2404.7z.188
+del ubuntu2404.7z.189
+del ubuntu2404.7z.190
+del ubuntu2404.7z.191
+del ubuntu2404.7z.192
+del ubuntu2404.7z.193
+del ubuntu2404.7z.194
+del ubuntu2404.7z.195
+del ubuntu2404.7z.196
+del ubuntu2404.7z.197
+del ubuntu2404.7z.198
+del ubuntu2404.7z.199
+del ubuntu2404.7z.200
+del ubuntu2404.7z.201
+del ubuntu2404.7z.202
+del ubuntu2404.7z.203
+del ubuntu2404.7z.204
+del ubuntu2404.7z.205
+del ubuntu2404.7z.206
+del ubuntu2404.7z.207
+del ubuntu2404.7z.208
+del ubuntu2404.7z.209
+del ubuntu2404.7z.210
+del ubuntu2404.7z.211
+del ubuntu2404.7z.212
+del ubuntu2404.7z.213
+del ubuntu2404.7z.214
+del ubuntu2404.7z.215
+del ubuntu2404.7z.216
+del ubuntu2404.7z.217
+del ubuntu2404.7z.218
+del ubuntu2404.7z.219
+del ubuntu2404.7z.220
+del ubuntu2404.7z.221
+del ubuntu2404.7z.222
+del ubuntu2404.7z.223
+del ubuntu2404.7z.224
+del ubuntu2404.7z.225
+del ubuntu2404.7z.226
+del ubuntu2404.7z.227
+del ubuntu2404.7z.228
+del ubuntu2404.7z.229
+del ubuntu2404.7z.230
+del ubuntu2404.7z.231
+del ubuntu2404.exe
+
+:::::::::::::::::::::::::::::::::::::::::::::
+(TIMEOUT /T 10)&&(pause)&&(exit /b)
+:::::::::::::::::::::::::::::::::::::::::::::
+:qwe
+	set var_File=%1
+	IF not EXIST %var_File% (
+		start https://github.com/IvanSibirevV2/Dist_Lin_Ubuntu24.04/raw/refs/heads/main/ubuntu2404/%var_File%
+	)
+	:qwer
+	IF not EXIST %var_File% (goto qwer)
+	IF EXIST %var_File% echo IsOk!-%var_File%
+exit /b
